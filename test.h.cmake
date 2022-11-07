c1058d41346551d71959b70671496b674c54eb16
Jan200101 <sentrycraft123@gmail.com>
2022-11-07 14:01:14 +0100

    base

---
+++ test.h.cmake
@@ -0,0 +1,21 @@
+
+// defined as "foo_content"
+#cmakedefine foo "@HAVE_FOO@" 
+
+// defined as 1
+#cmakedefine01 bar
+
+// defined with no value
+#cmakedefine foobar
+
+// defined as 0
+#cmakedefine01 barfoo
+
+// undefined
+#cmakedefine unfoo "@HAVE_FOO@"
+
+// undefined
+#cmakedefine unbar
+
+// defined as 0
+#cmakedefine01 unbarfoo
