c613082d1443015ef2c6a4a517dcbcd133e388a6
Jan200101 <sentrycraft123@gmail.com>
2023-01-23 20:45:14 +0100

    add diff script

---
+++ diff.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+MESON_BUILD_DIR="meson_build"
+CMAKE_BUILD_DIR="cmake_build"
+
+rm -rf $MESON_BUILD_DIR $CMAKE_BUILD_DIR
+
+meson setup "${MESON_BUILD_DIR}"
+cmake -S "." -B "${CMAKE_BUILD_DIR}"
+
+ninja -C "${MESON_BUILD_DIR}"
+cmake --build "${CMAKE_BUILD_DIR}"
+
+diff -u {${CMAKE_BUILD_DIR},${MESON_BUILD_DIR}}/test.h
