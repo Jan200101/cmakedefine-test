f8371518f211b60ff0edc0b178c1bb59792d05ad
Jan200101 <sentrycraft123@gmail.com>
2026-06-30 09:43:50 +0200

    add muon diff script

---
+++ muon_diff.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+MESON_BUILD_DIR="meson_build"
+CMAKE_BUILD_DIR="cmake_build"
+
+rm -rf $MESON_BUILD_DIR $CMAKE_BUILD_DIR
+
+muon setup "${MESON_BUILD_DIR}"
+cmake -S "." -B "${CMAKE_BUILD_DIR}"
+
+ninja -C "${MESON_BUILD_DIR}"
+cmake --build "${CMAKE_BUILD_DIR}"
+
+diff -w -u {${CMAKE_BUILD_DIR},${MESON_BUILD_DIR}}/test.h
+
+if [ "$?" -eq "0" ]; then
+    echo "Output matches"
+else
+    echo "Output does not match"
+fi
